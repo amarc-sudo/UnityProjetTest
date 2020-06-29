6f8807372aeb54f971f085da320a870fc01ea97e
Marc Aurelien <aurelienmarc2012@gmail.com>
2020-06-29 13:44:21 +0200

    e
    e

--- Assets/script/Controller2D.cs
+++ Assets/script/Controller2D.cs
@@ -4,33 +4,66 @@ using UnityEngine;
 
 public class Controller2D : MonoBehaviour
 {
-
+    [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;
+    private Vector3 m_Velocity = Vector3.zero;
     public float moveSpeed;
-    public float jumpForce = 300;
+    public float jumpForce;
+    [SerializeField]
+    private bool isJumping;
+    [SerializeField]
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask collisionLayers;
 
     public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
 
-    public Vector3 vector = Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 movement;
+    [SerializeField]
+    private float horizontalMovement;
 
-    public Boolean isJump = false;
-   
+    void Update() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, collisionLayers);
 
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
 
-    void FixedUpdate()
-    {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        if (Input.GetButtonDown("Jump"))
-            isJump = true;
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            isJumping = true;
+        }
+
+        Flip(rb.velocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+    }
+
+    void FixedUpdate() {
         MovePlayer(horizontalMovement);
     }
 
-    void MovePlayer(float _horizontalMovement){
-        Vector3 targetXVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
-        Vector3 vector3 = Vector3.SmoothDamp(rb.velocity, targetXVelocity, ref vector, .05f);
-        rb.velocity = vector3;
-        if (isJump){
+    void MovePlayer(float _horizontalMovement) {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement * 10f, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+        if (isJumping == true) {
             rb.AddForce(new Vector2(0f, jumpForce));
-            isJump = false;
+            isJumping = false;
         }
+    }
+
+    void Flip(float _velocity) {
+        if (_velocity > 0.1f) {
+            spriteRenderer.flipX = false;
+        } 
+        else if (_velocity < -0.1f) {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
---
+++ Assets/script/PauseManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour{
+
+    public Boolean isPause = false;
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            isPause = !isPause;
+        }
+        if (isPause)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+    public void pause() {
+        isPause = !isPause;
+    }
+}
